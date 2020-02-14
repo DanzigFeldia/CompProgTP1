68c0ece1f3faf62ef9514af6a543a69f3b609c0f
Danzig <sculteur.corentin@gmail.com>
2020-02-14 14:35:33 +0100

    first line is read

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from reader import reader
 
-R,C,F,N,B,T = reader("a_example.in")
+R,C,F,N,B,T,rides = reader("a_example.in")
 print(R)
 print(C)
 print(F)
 print(N)
 print(B)
 print(T)
+print(rides)
--- reader.py
+++ reader.py
@@ -3,6 +3,10 @@ def reader(filePath):
         file = f.read()
         lines = file.split("\n")
         (R,C,F,N,B,T) = str(lines[0]).split() #first line
+        rides = {}
+        i=0
+        for line in lines[1:len(lines)-1]:
+            rides[i] = str(line).split()
+            i += 1
 
-
-    return (R,C,F,N,B,T)
+    return (R,C,F,N,B,T, rides)
