d466cd1ba25f8a725473fe6f6055b5a3afad2930
Danzig <sculteur.corentin@gmail.com>
2020-02-14 17:06:42 +0100

    not working

--- car.py
+++ car.py
@@ -10,12 +10,13 @@ class Car:
         self.inRides = 0 #0 is not in ride, 1 is in ride
         self.target = (0,0)
         Car.nb_car += 1
+
     def assign(self,idRide, ride):
         self.occupation = idRide
-        self.target = (ride(0), ride(1))
+        self.target = (ride[0], ride[1])
     def update(self, rides):
-        if self.occupation != 1:
-            self.move(self.target)
+        if self.occupation != -1:
+            self.move(self.target[0],self.target[1])
             if self.pos == self.target:
                 if self.inRides == 0:
                     self.target = (rides[self.occupation][0],rides[self.occupation][1])
@@ -25,32 +26,40 @@ class Car:
                 else:
                     print("error")
         if self.occupation == -1:
-            closest = self.findClosestAcceptable()
+            closest = self.findClosestAcceptable(rides)
             if closest != -1:
-                self.assign(closest, rides(closest))
+                self.assign(closest, rides[closest])
+                rides[closest][6] = 1
             else:
                 print("stop")
         Car.step+=1
+        return rides
+
     def distance(self, a,b): #target is (x,y)
         return abs(self.pos[0]-a)+abs(self.pos[1]-b)
+
     def findClosestAcceptable(self, rides):
         for ride in rides:
             closest = (20001,-1) #distance, idRide
             timeneeded = abs(rides[ride][2]-rides[ride][0])+abs(rides[ride][3]-rides[ride][1])
             if rides[ride][6] == 0:
                 if self.distance(rides[ride][0],rides[ride][1]) < closest[0]:
-                    if self.distance(rides[ride][0],rides[ride][1])+timeneeded < rides[ride][5]:
+                    if self.distance(rides[ride][0],rides[ride][1])+timeneeded+Car.step < rides[ride][5]:
                         closest = (self.distance(rides[ride][0],rides[ride][1]),ride)
-        return closest[1]
-    def move(self,a,b):
-        while self.pos[0]!=a:
-            if self.pos[0] < a:
-                self.pos[0]+=1
-            elif self.pos[0] > a:
-                self.pos[0] -=1
-        while self.pos[1]!=b:
-            if self.pos[1] < b:
-                self.pos[1]+=1
-            elif self.pos[1] > b:
-                self.pos[1]-=1
 
+        return closest[1]
+
+    def move(self,a,b):
+        x = self.pos[0]
+        y = self.pos[1]
+        if x!=a:
+            if x < a:
+                x += 1
+            elif x > a:
+                x -=1
+        if y!=b:
+            if y < b:
+                y+=1
+            elif y > b:
+                y-=1
+        self.pos = (x,y)
--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from reader import reader
+from writer import writer
+from car import Car
 
 if __name__ == "__main__":
     R,C,F,N,B,T,rides = reader("a_example.in")
-    print(R)
-    for ride in rides:
-        print(rides[ride])
+
+    carList = []
+    for i in range(0,F):
+        carList.append(Car())
+    while Car.step < T:
+        isCarFunc = 0
+        for car in carList:
+            rides = car.update(rides)
+
+    writer("result.in", carList)
