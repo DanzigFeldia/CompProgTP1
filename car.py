ff7d1b4feb43792bd8d71e32a48783d92e7668dd
Danzig <sculteur.corentin@gmail.com>
2020-02-17 13:53:18 +0100

    not the best score but should be final

--- car.py
+++ car.py
@@ -1,18 +1,20 @@
 class Car:
     nb_car = 0
     step = 0
+    maxStep = 0
     score = 0
     bonus = 0
-    bonusMultiplier = 0.5 #the lower it is, the more important bonuses are
+    bonusMultiplier = 1 #the lower it is, the more important bonuses are (0<x<=1)
 
-    def __init__(self, B):
+    def __init__(self, B, T):
         self.id = Car.nb_car
         self.pos = (0, 0)
         self.rCompleted = []
         self.occupation = -1
         self.inRides = 0 #0 is not in ride, 1 is in ride
         self.target = (0,0)
-        self.stopped = 0
+        self.stopped = 0 #1 when there's no rides anymore
+        Car.maxStep = T
         Car.bonus = B
         Car.nb_car += 1
 
@@ -20,6 +22,7 @@ class Car:
         self.occupation = idRide
         self.target = (ride[0], ride[1])
         Car.score+=abs(ride[2]-ride[0])+abs(ride[3]-ride[1])
+        ride[6] = 1
         if (Car.step+self.distance(ride[0],ride[1])) <= ride[4]:
             Car.score += Car.bonus
     def update(self, rides):
@@ -28,7 +31,7 @@ class Car:
                 self.move(self.target[0],self.target[1])
                 if self.pos == self.target:
                     if self.inRides == 0:
-                        self.target = (rides[self.occupation][0],rides[self.occupation][1])
+                        self.target = (rides[self.occupation][2],rides[self.occupation][3])
                         self.inRides = 1
                     elif self.inRides == 1:
                         self.inRides =0
@@ -37,10 +40,10 @@ class Car:
                     else:
                         print("error")
         if self.occupation == -1 and self.stopped == 0:
-            closest = self.findClosestAcceptable(rides)
+            closest = self.findMostProfitable(rides)
             if closest != -1:
                 self.assign(closest, rides[closest])
-                rides[closest][6] = 1
+
             elif closest == -1:
                 self.stopped = 1
 
@@ -51,16 +54,33 @@ class Car:
         return abs(self.pos[0]-a)+abs(self.pos[1]-b)
 
     def findClosestAcceptable(self, rides):
-        closest = (20001, -1)  # distance, idRide
+        closest = (999999999999999999, -1)  # distance, idRide
         for ride in rides:
             if rides[ride][6] == 0:
                 distance = self.distance(rides[ride][0],rides[ride][1])
                 if (self.distance(rides[ride][0],rides[ride][1])+Car.step) <= rides[ride][4]:
                     distance *= Car.bonusMultiplier
-                if distance < closest[0] and ((self.distance(rides[ride][0],rides[ride][1])+abs(rides[ride][2] - rides[ride][0]) + abs(rides[ride][3] - rides[ride][1])+Car.step) <= rides[ride][5]):
+                timeNeeded = (self.distance(rides[ride][0],rides[ride][1])+abs(rides[ride][2] - rides[ride][0]) + abs(rides[ride][3] - rides[ride][1])+Car.step)
+                if distance < closest[0] and timeNeeded <= rides[ride][5] and timeNeeded <= Car.maxStep:
                     closest = (self.distance(rides[ride][0],rides[ride][1]),ride)
 
         return closest[1]
+
+    def findMostProfitable(self, rides):
+        mostProfitables = (0, -1)
+        for ride in rides:
+            if rides[ride][6] == 0:
+                score = 0
+                distance = self.distance(rides[ride][0], rides[ride][1])
+                timeNeeded = (distance + abs(rides[ride][2] - rides[ride][0]) + abs(rides[ride][3] - rides[ride][1]))+Car.step
+                if timeNeeded <= rides[ride][5] and timeNeeded <= Car.maxStep:
+                    score += abs(rides[ride][2] - rides[ride][0]) + abs(rides[ride][3] - rides[ride][1])
+                    if distance + Car.step <= rides[ride][4]:
+                        score += Car.bonus
+                    if (score/timeNeeded) > mostProfitables[0]:
+                        mostProfitables =((score/timeNeeded),ride)
+        return mostProfitables[1]
+
 
     def move(self,a,b):
         x = self.pos[0]
@@ -70,7 +90,7 @@ class Car:
                 x += 1
             elif x > a:
                 x -=1
-        if y!=b:
+        elif y!=b:
             if y < b:
                 y+=1
             elif y > b:
--- main.py
+++ main.py
@@ -3,20 +3,21 @@ from writer import writer
 from car import Car
 
 if __name__ == "__main__":
-    R,C,F,N,B,T,rides = reader("e_high_bonus.in")
+    with open("log.in", "w") as log:
+        R,C,F,N,B,T,rides = reader("e_high_bonus.in")
 
-    carList = []
-    for i in range(0,F):
-        carList.append(Car(B))
-    while Car.step < T:
-        print(Car.step)
-        isCarFunc = 0
-        for car in carList:
-            car.update(rides)
-            if car.occupation != -1:
-                isCarFunc +=1
-        if isCarFunc == 0:
-            break
-        Car.step +=1
-    writer("result.in", carList)
-    print(Car.score)
+        carList = []
+        for i in range(0,F):
+            carList.append(Car(B, T))
+        while Car.step < T:
+            isCarFunc = 0
+            for car in carList:
+                car.update(rides)
+                log.write("Car : " + str(car.id) + " - rides : " + str(car.occupation) + " - position : " + str(car.pos) + " - target : " + str(car.target) + "\n")
+                if car.stopped != 1:
+                    isCarFunc +=1
+            if isCarFunc == 0:
+                break
+            Car.step +=1
+        writer("result.in", carList)
+        print(Car.score)
